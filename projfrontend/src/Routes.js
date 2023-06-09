e52de903361197f3924d1fd17e14b8ce72662273
Jainendra Dwivedi <dwivedijainendra111@gmail.com>
2023-06-09 15:42:41 +0530

    CRUD: products complete (delete and update)

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -10,7 +10,8 @@ import AdminDashBoard from "./user/AdminDashBoard"
 import AddCategory from "./admin/AddCategory";
 import ManageCategories from "./admin/ManageCategories";
 import AddProduct from "./admin/AddProduct";
-
+import ManageProducts from "./admin/ManageProducts";
+import UpdateProduct from "./admin/UpdateProduct";
 
 
 const Routes = () => {
@@ -25,6 +26,8 @@ const Routes = () => {
         <AdminRoute path="/admin/create/category" exact component={AddCategory}/>
         <AdminRoute path="/admin/categories" exact component={ManageCategories}/>
         <AdminRoute path="/admin/create/product" exact component={AddProduct} /> 
+        <AdminRoute path="/admin/products" exact component={ManageProducts} />
+        <AdminRoute path="/admin/product/update/:productId" exact component={UpdateProduct} />
       </Route>
     </BrowserRouter>
   );
--- projfrontend/src/admin/ManageCategories.js
+++ projfrontend/src/admin/ManageCategories.js
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Base from "../core/Base";
+import { Link } from "react-router-dom";
+import { isAutheticated } from "../auth/helper";
+import { getCategories } from "./helper/adminapicall";
 
 const ManageCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  const { user, token } = isAutheticated();
+
+  const preload = () => {
+    getCategories().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    preload();
+  }, []);
+
   return (
-    <Base
-      title="Manage categories here"
-      description="manage kro bhaiya ji sab"
-      className="container bg-info p-4"
-    >
-      <div className="row bg-white rounded">
-        <div className="col-md-8 offset-md-2">
-            <h1>TODO</h1>
+    <Base title="Welcome admin" description="Manage categories here">
+      <h2 className="mb-4">All Categories:</h2>
+      <Link className="btn btn-info" to={`/admin/dashboard`}>
+        <span className="">Admin Home</span>
+      </Link>
+      <div className="row">
+        <div className="col-12">
+          <h2 className="text-center text-white my-3">Total Categories</h2>
+          {categories.map((category, index) => {
+            return (
+            <div key={index} className="row text-center mb-2 ">
+            <div className="col-4">
+              <h3 className="text-white text-left">{category.name}</h3>
+            </div>
+            <div className="col-4">
+              <Link
+                className="btn btn-success"
+                to={`/admin/product/update/productId`}
+              >
+                <span className="">Update</span>
+              </Link>
+            </div>
+            <div className="col-4">
+              <button onClick={() => {}} className="btn btn-danger">
+                Delete
+              </button>
+            </div>
+          </div>
+            );
+          })}
         </div>
       </div>
     </Base>
